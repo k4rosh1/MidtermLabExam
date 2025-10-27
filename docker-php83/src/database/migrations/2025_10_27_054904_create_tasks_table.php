c6bb8d9585432a39a0e78d8ba52ead202f9efd7a
Ramon <ramoncarlospacilona22@gmail.com>
2025-10-27 14:42:49 +0800

    Task3

--- docker-php83/src/database/migrations/2025_10_27_054904_create_tasks_table.php
+++ docker-php83/src/database/migrations/2025_10_27_054904_create_tasks_table.php
@@ -9,10 +9,15 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
+  
     public function up(): void
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('status')->default('pending'); // Using string as per your spec
+            $table->date('due_date')->nullable();
             $table->timestamps();
         });
     }
